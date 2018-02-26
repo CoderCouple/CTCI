56040119a94a73c16373b9794096ee341dd7c694
Sunil Tiwari <sunil28071987@gmail.com>
2018-02-25 22:16:23 -0800

    Create StackOfStacks.java

---
+++ Stacks-Queue/StackOfStacks.java
@@ -0,0 +1,32 @@
+package StacksAndQueues;
+
+import java.util.Stack;
+
+public class StackOfStacks {
+
+	Stack<Integer> s;
+	Stack<Stack> sos;
+	
+	public StackOfStacks(){
+		s = new Stack<Integer>();
+		sos = new Stack<Stack>();
+	}
+	
+	public void push(){
+		
+	}
+	
+	public int pop(){
+		return 0;
+	}
+	
+	public int peek(){
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
