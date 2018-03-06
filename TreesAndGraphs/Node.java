0790347608468342e89577cc9c6c7110e2c69ea3
Sunil Tiwari <sunil28071987@gmail.com>
2018-03-05 16:19:36 -0800

    Create Node.java

---
+++ TreesAndGraphs/Node.java
@@ -0,0 +1,16 @@
+package Trees;
+
+public class Node {
+
+	int val;
+	Node left;
+	Node right;
+	Node parent;
+
+	public Node(int data) {
+		val = data;
+		left = null;
+		right = null;
+		parent = null;
+	}
+}
