be25662de7c46ef4f9f5ac23ee7296a152337b02
Sunil Tiwari <sunil28071987@gmail.com>
2018-06-11 00:55:37 -0700

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -15,10 +15,28 @@ public class BubbleSort {
 			}
 	}
 	
+	public static void modifiedBubbleSort(int [] nums){
+		boolean isSorted=false;
+		int range= nums.length-1;
+		while(!isSorted){
+			isSorted=true;
+			for(int i=0;i<range;i++){
+				if(nums[i]>nums[i+1]){
+					int temp = nums[i+1];
+					nums[i+1]=nums[i];
+					nums[i]=temp;
+					isSorted=false;
+				}
+			}
+			range--;
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int array []={7,4,1,10,5,100,2};
-		bubbleSort(array);
+		//bubbleSort(array);
+		modifiedBubbleSort(array);
 		System.out.println(Arrays.toString(array));
 
 	}
