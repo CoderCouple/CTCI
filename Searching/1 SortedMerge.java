dbec273a3259db456f9bdd9e0f49d648bf0f2681
Sunil Tiwari <sunil28071987@gmail.com>
2018-06-20 02:02:59 -0700

    Create 1 SortedMerge.java

---
+++ Searching/1 SortedMerge.java
@@ -0,0 +1,49 @@
+package SearchingAndSorting;
+
+public class SortedMerge {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int [] large=nums1;
+        int [] small=nums2;
+        int l=m-1;
+        int s=n-1;
+
+        // if(nums1.length>=nums1.length){
+        //     large=nums1;
+        //     l=m-1;
+        //     small=nums2;
+        //     s=n-1;
+        // } else {
+        //     large=nums2;
+        //     l=n-1;
+        //     small=nums1;
+        //     s=m-1;
+        // }
+
+        int counter=large.length-1;
+        while(s>=0 && l>=0){
+            if(small[s]>=large[l]){
+                large[counter]=small[s];
+                s--;
+            } else {
+                large[counter]=large[l];
+                l--;
+            }
+            counter--;
+        }
+
+        while(l>=0 && counter>=0){
+            large[counter]=large[l];
+            l--;
+            counter--;
+        }
+
+        while(s>=0 && counter>=0){
+            large[counter]=small[s];
+            s--;
+            counter--;
+        }
+
+    }
+
+}
