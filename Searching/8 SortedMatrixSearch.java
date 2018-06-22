a55f7ebd3c700c42c41cb848af10b9b976f280a6
Sunil Tiwari <sunil28071987@gmail.com>
2018-06-22 16:04:42 -0700

    Update 8 SortedMatrixSearch.java

--- Searching/8 SortedMatrixSearch.java
+++ Searching/8 SortedMatrixSearch.java
@@ -2,12 +2,9 @@ package SearchingAndSorting;
 
 public class SortedMatrixSearch {
 
-
     public static int searchSortedMatrix1(int [][] matrix,int key){
-
         if(matrix.length==0 || matrix==null)
             return -1;
-
         for(int i=0; i<matrix.length;i++){
             int start=0;
             int end=matrix[i].length-1;
@@ -17,13 +14,10 @@ public class SortedMatrixSearch {
                     return index;
             }
         }
-
-
         return -1;
     }
 
     public static int binary1DSearch(int [] matrix,int key,int start,int end){
-
         while (start<=end){
             int mid=(start+end)/2;
             if(matrix[mid]==key)
@@ -35,61 +29,24 @@ public class SortedMatrixSearch {
         return -1;
     }
 
-
-package SearchingAndSorting;
-
-public class SortedMatrixSearch {
-
-
-    public static int searchSortedMatrix1(int [][] matrix,int key){
-
-        if(matrix.length==0 || matrix==null)
-            return -1;
-
-        for(int i=0; i<matrix.length;i++){
-            int start=0;
-            int end=matrix[i].length-1;
-            if(matrix[i].length>0 && matrix[i][start]<=key || key<=matrix[i][end]){
-                int index = binary1DSearch(matrix[i],key,start,end);
-                if(index!=-1)
-                    return index;
-            }
+    public static boolean searchSortedMatrix2(int [][] matrix,int key,int startRow,int startCol, int endRow, int endCol){
+        while (startCol>=endCol || startRow>=endRow) {
+            if (matrix[startRow][startCol] == key)
+                return true;
+            else if (matrix[startRow][startCol] > key)
+                startCol--;
+            else
+                startRow--;
         }
-
-
-        return -1;
-    }
-
-    public static int binary1DSearch(int [] matrix,int key,int start,int end){
-
-        while (start<=end){
-            int mid=(start+end)/2;
-            if(matrix[mid]==key)
-                return mid;
-            else if(matrix[mid]<key)
-                start=mid+1;
-            else end=mid-1;
-        }
-        return -1;
+        return false;
     }
 
     public static void main(String args []){
-
         int [][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
         int key=7;
 
         System.out.println(searchSortedMatrix1(matrix,key));
+        assert searchSortedMatrix2(matrix,8,0,matrix[0].length-1,matrix.length-1,0) : "Test Case 1 Failed";
+        assert searchSortedMatrix2(matrix,10,0,matrix[0].length-1,matrix.length-1,0) : "Test Case 2 Failed";
     }
-
-}
-
-
-    public static void main(String args []){
-
-        int [][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
-        int key=7;
-
-        System.out.println(searchSortedMatrix1(matrix,key));
-    }
-
 }
